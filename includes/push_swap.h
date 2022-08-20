f16716b1bc8cd27130d07935e3d24e323bb14e45
Alexis R <64582375+alexisradice@users.noreply.github.com>
2022-08-20 20:26:02 +0200

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -10,7 +10,7 @@ typedef struct s_stack
 {
 	int				value;
 	int				index;
-	int				pos;
+	int				position;
 	int				target_pos;
 	int				cost_a;
 	int				cost_b;
@@ -21,9 +21,10 @@ t_stack	*ft_new_stack(int content);
 void	ft_init_all(t_stack **stack, int argc, char **argv);
 void	ft_index(t_stack *stack, int argc);
 void	ft_small_sort(t_stack **stack);
-// void	ft_push_swap(t_stack **stack_a, t_stack **stack_b, int argc);
-void	ft_push_swap(t_stack **stack_a, int argc);
-void	ft_push(t_stack *stack_dest, t_stack *stack_src);
+void	ft_big_sort(t_stack **stack_a, t_stack **stack_b, int argc);
+void	ft_set_position(t_stack **stack);
+void	ft_push_swap(t_stack **stack_a, t_stack **stack_b, int argc);
+void	ft_push(t_stack **stack_dest, t_stack **stack_src);
 void	ft_swap(t_stack **stack);
 t_stack	*ft_stack_last(t_stack *stack);
 t_stack	*ft_stack_before_last(t_stack *stack);
@@ -32,6 +33,5 @@ void	ft_reverse_rotate(t_stack **stack);
 void	ft_display_stack(t_stack *stack);
 void	ft_free_all(t_stack *stack);
 int		main(int argc, char **argv);
-
 
 #endif
