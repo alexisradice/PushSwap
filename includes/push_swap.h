07e07eafb91d1970ed35a936cca8f81a8e9bc444
Alexis R <64582375+alexisradice@users.noreply.github.com>
2022-08-21 20:10:58 +0200

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -11,7 +11,7 @@ typedef struct s_stack
 	int				value;
 	int				index;
 	int				position;
-	int				target_pos;
+	int				target_position;
 	int				cost_a;
 	int				cost_b;
 	struct s_stack	*next;
@@ -22,6 +22,7 @@ void	ft_init_all(t_stack **stack, int argc, char **argv);
 void	ft_index(t_stack *stack, int argc);
 void	ft_small_sort(t_stack **stack);
 void	ft_big_sort(t_stack **stack_a, t_stack **stack_b, int argc);
+void	ft_set_target_position(t_stack **stack_a, t_stack **stack_b);
 void	ft_set_position(t_stack **stack);
 void	ft_push_swap(t_stack **stack_a, t_stack **stack_b, int argc);
 void	ft_push(t_stack **stack_dest, t_stack **stack_src);
